1136c1f75add3568fcf2731db7caeb02d884796a
AliStoltz <ali.mstoltz@gmail.com>
2020-02-25 15:31:25 -0700

    routes

---
+++ tonys-app/src/config/Routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { directive } from '@babel/types';
+
+export default ({}) => (
+  <div className="routes">
+    <Switch>
+      <Route />
+    </Switch>
+  </div>
+);
